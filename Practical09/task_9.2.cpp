6aa3bdec65f216e7dc13b5ca4cf426bd9d0b8c53
Shikhar-dhyani <79635946+Shikhar-dhyani@users.noreply.github.com>
2021-11-04 06:46:22 -0400

    Update task_9.2.cpp

--- Practical09/task_9.2.cpp
+++ Practical09/task_9.2.cpp
@@ -1,21 +1,20 @@
 #include<iostream>
+#include<string>
 using namespace std;
-class Area{
-    public:
-float area(float l,float b)
+class cal_area{
+public:
+ cal_area(int a)
 {
-    return l*b;
-}
-float area(float side)
-{
-    return side*side;
-}
+  int area=4*(a)*(a);
+  cout<<"area of square"<<" "<<area<<endl;
+  }
+   cal_area(int l,int b)
+  {
+  int area=l*b;
+  cout<<"area of rectangle"<<" "<<area<<endl;
+  }
 };
-int main(){
-   Area square,rectangle;
-  float sq= square.area(67.00,2.00);
-  float rec=square.area(12);
-  cout << "Area of square is "<<sq <<endl;
-  cout << "Area of rectangle is "<<rec <<endl;
-  return 0;
-}
+int main()
+{
+  cal_area A1(4),A2(3,4);
+ }
