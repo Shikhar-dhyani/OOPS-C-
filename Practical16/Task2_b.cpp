d6c57403910bd2dc2628c235345d9c8b71b8866a
Shikhar Dhyani <shikhardhyani025@gmail.com>
2022-01-07 19:34:51 +0530

    Update Task2_b.cpp

--- Practical16/Task2_b.cpp
+++ Practical16/Task2_b.cpp
@@ -18,3 +18,6 @@ int main()
     }
     return 0;
 }
+//The block catch(...) is used for catch all, when a data type of a thrown exception doesn't 
+//match with any other catch block, the code inside catch(...) is executed. Note that the implicit type conversion doesn't happen
+//when exceptions are caught. The character 'a' is not automatically converted to int.
